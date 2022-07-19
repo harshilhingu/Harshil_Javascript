6794469ad8af6f74101987b4042d2f2a38e87aea
harshilhingu <91302645+harshilhingu@users.noreply.github.com>
2022-07-19 10:40:34 -0400

    assignment-5

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Content } from '../models/content';
+import { BikersService } from '../services/bikers.service';
 
 @Component({
   selector: 'app-content-list',
@@ -12,88 +13,45 @@ export class ContentListComponent implements OnInit {
     message: '',
     found: false,
   };
-  constructor() {
-    this.Bikeslist = [
-      {
-        id: 0,
-        title: 'CBR 10000RR',
-        body: 'steel',
-        imageLink:
-          'https://cdni.autocarindia.com/ExtraImages/20200730063629_2020-Honda-CBR1000RR-R-Fireblade-SP.jpg',
-        type: 'Race bike',
-        author: 'tata motors',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-
-      {
-        id: 1,
-        title: 'BMW',
-        body: 'steel',
-        imageLink:
-          'https://img.etimg.com/thumb/msid-75572296,width-640,resizemode-4,imgsize-507941/bmw-ninet.jpg',
-        type: 'Race bike',
-        author: 'tata motors',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 3,
-        title: 'Splender',
-        body: 'steel',
-        imageLink:
-          'https://bd.gaadicdn.com/processedimages/hero-motocorp/hero-motocorp-splendor/source/hero-motocorp-splendor62863b5bef195.jpg',
-        type: 'Heavy model',
-        author: 'tesla',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 3,
-        title: 'Royal Enfeild',
-        body: 'whole steel',
-        imageLink: 'https://images.hindustantimes.com/auto/img/2021/01/13/600x338/Royal_Enfiled_Classic_350_1606287446412_1606287452623_1610507147713.jpg',
-        type: 'Heavy model',
-        author: 'tata motors',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 4,
-        title: 'Hero Honda',
-        body: 'aluminum',
-        imageLink:
-          'https://static.autox.com/uploads/bikes/2017/05/1486030508-hero-hf-deluxe.jpg',
-        author: 'tata motors',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 5,
-        title: 'Hayabuza',
-        body: 'aluminum',
-        imageLink:
-          'https://img.etimg.com/thumb/msid-82267745,width-1070,height-580,imgsize-584129,overlay-etpanache/photo.jpg',
-        type: '',
-        author: 'tesla',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 6,
-        title: 'Trigger',
-        body: 'aluminum',
-        imageLink:
-          'https://etimg.etb2bimg.com/thumb/msid-48356059,imgsize-83923,width-1200,height-628,overlay-etauto/honda-motorcycle-and-scooter-india-discontinues-cb-trigger-within-30-months-of-launch.jpg',
-        // type:
-        author: 'tesla',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-      {
-        id: 7,
-        title: 'Ola',
-        body: 'aluminum',
-        author: 'tesla',
-        hashtags: ['BMW', 'Trigger', 'Hayabuza'],
-      },
-    ];
+  hikaru: Content = {
+    id: 3,
+    title: 'Sam',
+    body: 'A Biker  who became a successful Actor instead',
+    author: 'Harshil',
+    type: 'GM',
+    hashtags: ['supergm', 'indepthanalysis'],
+  };
+  types: string[] = ['', 'IM', 'FM', 'GM'];
+  constructor(private BikerService: BikersService) {
+    this.Bikeslist = [];
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // getContent test
+    this.BikerService.getContent().subscribe(
+      (BikersArray) => (this.Bikeslist = BikersArray)
+    );
+
+    // getContentItem test - worked
+    // this.BikerService.getContentItem(1).subscribe(BikerSingleItem => {
+    //   console.log("Got the content item: ", BikerSingleItem);
+    // });
+
+    // addContentItem test - worked
+    // this.chessChampionService.addContentItem(this.hikaru).subscribe(chessChampionsArray => {
+    //   console.log("Added Hikaru: ", chessChampionsArray);
+    // });
+
+    // updateContentItem test - worked
+    // this.chessChampionService.updateContentItem(this.hikaru).subscribe(chessChampionsArray => {
+    //   console.log("updated Hikaru: ", chessChampionsArray);
+    // });
+
+    // deleteContentItem test - worked
+    // this.chessChampionService.deleteContentItem(1).subscribe(chessChampionsSingleItem => {
+    //   console.log("Deleted the content item: ", chessChampionsSingleItem);
+    // });
+  }
   checkForAuthorInList(authorNameValue: string): void {
     if (
       this.Bikeslist.some(
